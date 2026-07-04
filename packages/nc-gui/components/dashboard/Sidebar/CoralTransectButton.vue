5e01cb9e1441b2a005f598364665b2bea823fd0c
Boris <uniboris1@gmail.com>
2026-07-04 12:36:34 +0300

    remove WIP coral transects option from sidebar

--- packages/nc-gui/components/dashboard/Sidebar/CoralTransectButton.vue
+++ packages/nc-gui/components/dashboard/Sidebar/CoralTransectButton.vue
@@ -9,12 +9,6 @@ const openCoralTransectEditor = async () => {
 
   await router.push(`/nc/${base.value.id}/coral-transects`)
 }
-
-const openWipCoralTransects = async () => {
-  if (!base.value?.id) return
-
-  await router.push(`/nc/${base.value.id}/coral-transects?tab=wip`)
-}
 </script>
 
 <template>
@@ -28,17 +22,6 @@ const openWipCoralTransects = async () => {
         <GeneralIcon icon="ncTable" class="flex-none h-5 w-5" />
       </template>
       <span>Coral transects</span>
-    </NcSidebarMenuItem>
-
-    <NcSidebarMenuItem
-      class="group !my-0 !h-10 !gap-3 !text-sm"
-      data-testid="nc-coral-transect-wip-launcher"
-      @click="openWipCoralTransects()"
-    >
-      <template #icon>
-        <GeneralIcon icon="ncClock" class="flex-none h-5 w-5" />
-      </template>
-      <span>WIP transect reports</span>
     </NcSidebarMenuItem>
   </div>
 </template>
